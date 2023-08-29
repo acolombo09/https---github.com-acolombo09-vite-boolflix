dc3289952f1343b24b5e69938c4bb646f7c8a2e8
acolombo09 <acolombo0911@gmail.com>
2023-08-29 18:04:18 +0200

    milestone_1_reviewed+milestone_2_half-done

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -4,7 +4,7 @@
 export default {
   props: {
 // creare la prop per ricevere i film trovati e stamparli in html nella card
-  movieData: Object
+  mediaData: Object,
   },
   data() {
     return {
@@ -12,7 +12,8 @@ export default {
   },
   methods:{
 
-  }
+  },
+  
 };
 </script>
 
@@ -21,24 +22,13 @@ export default {
     <div class="card border-0 rounded-0 bg-transparent">
       <img src="../assets/vue.svg" style="height: 50px;" class="card-img-top rounded-0" alt="">
       <div class="card-body px-0 text-center">
-        <h5 class="text-white">{{movieData.title}}</h5>
-        <h6 class="text-white">Titolo Originale: {{movieData.original_title}}</h6>
-        <h6 class="text-white">Lingua Originale: {{movieData.original_language}}</h6>
-        <h6 class="text-white">Voti: {{movieData.vote_count}}</h6>
+        <h5 class="text-white">{{ mediaData.title || mediaData.name }}</h5>
+        <h6 class="text-white">Titolo Originale: {{ mediaData.original_title || mediaData.original_name }}</h6>
+        <h6 class="text-white">Lingua Originale: {{ mediaData.original_language }}</h6>
+        <h6 class="text-white">Voti: {{ mediaData.vote_average }}</h6>
       </div>
     </div>
   </div>
-    <!-- v-for da inserire in themain.vue -->
-  <!-- <div class="col" v-for="(result, i) in resultsList" v-bind:key="`result_${i}`">
-    <div class="card border-0 rounded-0 bg-transparent">
-      <img v-bind:src="result.poster_path" class="card-img-top rounded-0" alt="">
-      <div class="card-body px-0">
-        <h5 class="text-white">
-          {{ result.title }}
-        </h5>
-      </div>
-    </div>
-  </div> -->
 </template>
 
 <style lang="scss" scoped>
--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -3,11 +3,10 @@ import axios from "axios";
 import AppCard from "./AppCard.vue";
 // importo solo la variabile store, per questo l'ho messo fuori dal default
 import { store } from "../store";
-// searchText lo inserisco direttamente nello store.js
 
 export default {
   props: {
-    movieData: Object
+    mediaData: Object,
   },
   components: {
     AppCard,
@@ -16,28 +15,26 @@ export default {
     return {
       store,
       searchQuery: "",
-      searchResults: []
+      searchResults: [],
     };
   },
   methods: {
-    searchMovies() {
-      // esempio link chiamata
-      // https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+futuro
+    searchMedia() {
       const apiKey = "65ff2578e5da8719abf8bf6464f1c406";
-      const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${this.searchQuery}`;
+      const url = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${this.searchQuery}`;
+
       axios.get(url)
-      .then((response) => {
-        // una volta recuperati i dati dal server, li salvo nel data
-        this.searchResults = response.data.results;
-        console.log(this.searchResults);
-      })
+        .then((response) => {
+          this.searchResults = response.data.results;
+          console.log(this.searchResults);
+        })
         .catch(error => {
           console.error(error);
-      });
+        });
     },
   },
   mounted() {
-    this.searchMovies();
+    this.searchMedia();
   }
 };
 </script>
@@ -56,41 +53,15 @@ export default {
               <div class="input-group">
                 <input type="text" class="form-control" placeholder="Cerca film..." v-model="searchQuery"/>
                 <button class="btn btn-outline-light" type="button" id="button-addon2"
-                @click="searchMovies">Cerca</button>
+                @click="searchMedia">Cerca</button>
               </div>
             </li>
           </ul>
         </div>
       </div>
       <div class="row row-cols-4 mx-auto py-3">
-        
-        <AppCard v-for="movie in searchResults" v-bind:key="movie.id" v-bind:movieData="movie"></AppCard>
-
+        <AppCard v-for="media in searchResults" :key="media.id" :mediaData="media"></AppCard>
       </div>
-      <!-- Sezione ricerca serie TV -->
-      <!-- <div class="row searchbar">
-        <div class="col mx-auto py-3">
-          <ul class="nav nav-pills d-flex justify-content-between align-items-center">
-            <li class="nav-item">
-              <h4 class="text-white">MOVIES</h4>
-            </li>
-            <li class="nav-item">
-              <div class="input-group">
-                <input type="text" class="form-control" placeholder="Ricerca" v-model="store.searchText"/>
-                <button class="btn btn-outline-light" type="button" id="button-addon2"
-                @click="performSearch">
-                  Cerca
-                </button>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="row row-cols-6 mx-auto py-3">
-        <div class="col d-flex justify-content-center">
-          <AppCard></AppCard>
-        </div>
-      </div> -->
     </div>
   </main>
 </template>
