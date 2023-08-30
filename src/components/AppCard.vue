05f8084b0e9639cc26a56ecf6cb8797c3eab60cb
acolombo09 <acolombo0911@gmail.com>
2023-08-30 12:55:51 +0200

    milestone_2&3-half_completed

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -5,19 +5,44 @@ export default {
   // creare la prop per ricevere i film trovati e stamparli in html nella card
     mediaData: Object,
   },
-  
+  methods: {
+    // creazione funzione trova poster e gestione della card senza poster
+    getImageUrl() {
+      if(!this.mediaData.poster_path) {
+        return "/placeholder-movie.png";
+      }
+        return "https://image.tmdb.org/t/p/w342" + this.mediaData.poster_path;
+    }
+  }
 };
+
+/*
+"poster_sizes": [
+  "w92",
+  "w154",
+  "w185",
+  "w342",
+  "w500",
+  "w780",
+  "original"
+],
+*/
+
 </script>
 
 <template>
   <div class="col-3 d-flex justify-content-center">
-    <div class="card border-0 rounded-0 bg-transparent">
-      <img src="../assets/vue.svg" style="height: 50px;" class="card-img-top rounded-0" alt="">
+    <div class="card border-0 rounded-0 bg-transparent h-100" style="max-width: 300px;">
+      <!-- dopo aggiungi " -->
+      <img :src="getImageUrl()" style="max-height: 342px; aspect-ratio: auto;" class="card-img-top rounded-0 poster-img">
       <div class="card-body px-0 text-center">
         <h5 class="text-white">{{ mediaData.title || mediaData.name }}</h5>
         <h6 class="text-white">Titolo Originale: {{ mediaData.original_title || mediaData.original_name }}</h6>
         <h6 class="text-white">Lingua Originale: {{ mediaData.original_language }}</h6>
-        <h6 class="text-white">Voti: {{ mediaData.vote_average }}</h6>
+        <h6 class="text-white">
+          Voto: {{ Math.ceil(mediaData.vote_average / 2) + "/5" }}
+          <i class="fa-solid fa-star text-warning"></i>
+        </h6>
       </div>
     </div>
   </div>
