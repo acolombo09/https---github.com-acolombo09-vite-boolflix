dfd75b84ec8748f0de8b9b707dd01beed04b6958
acolombo09 <acolombo0911@gmail.com>
2023-08-30 13:32:45 +0200

    header_style_contents-added

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -12,19 +12,55 @@ export default {
 </script>
 
 <template>
-  <header class="bg-black py-3">
+  <!-- <header class="bg-black py-3">
     <div class="container-fluid">
-      <div class="row searchbar">
+      <div class="row">
         <div class="col mx-auto">
           <ul class="nav nav-pills d-flex justify-content-between align-items-center">
             <li class="nav-item">
-              <h3 class="text-danger">BOOLFLIX</h3>
+              
             </li>
           </ul>
         </div>
       </div>
     </div>
+  </header> -->
+
+  <header class="bg-black py-3">
+    <div class="container-fluid">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="#" class="d-flex align-items-center link-body-emphasis text-decoration-none me-5">
+          <h3 class="text-danger">BOOLFLIX</h3>
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="#" class="nav-link px-2 link-light">Home</a></li>
+          <li><a href="#" class="nav-link px-2 link-light">TV Series</a></li>
+          <li><a href="#" class="nav-link px-2 link-light">Movies</a></li>
+          <li><a href="#" class="nav-link px-2 link-light">Originals</a></li>
+          <li><a href="#" class="nav-link px-2 link-light">Recently Added</a></li>
+          <li><a href="#" class="nav-link px-2 link-light">My list</a></li>
+        </ul>
+
+        <!-- <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+          <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+        </form> -->
+
+        <div class="dropdown text-end">
+          <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle text-light" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+          </a>
+          <ul class="dropdown-menu text-small">
+            <li><a class="dropdown-item" href="#">Settings</a></li>
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Sign out</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </header>
+
 </template>
 
 <style lang="scss" scoped>
@@ -35,9 +71,6 @@ header{
   min-height: 75px;
   // background-color: $color-secondary;
 
-  // img {
-  //   aspect-ratio: 1/1;
-  // }
 }
 
 </style>
